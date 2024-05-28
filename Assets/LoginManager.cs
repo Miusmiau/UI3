d14c9bdedf55a050e2a400952f4daa1bc093cf69
Miusmiau <164009952+Miusmiau@users.noreply.github.com>
2024-05-28 09:51:01 -0300

    ejercicio completo

---
+++ Assets/LoginManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class LoginManager : MonoBehaviour
+{
+    public string Password;
+    public InputField input_password;
+
+    // Start is called before the first frame update
+    public void Checkpass()
+    {
+        if (Password == input_password.text)
+        {
+            Debug.Log(" Access Granted ");
+
+        }
+        else
+        {
+            Debug.Log("Access Denied");
+        }
+
+
+    }
+}
